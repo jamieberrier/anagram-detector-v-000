1c0b0e33020b06e1ccc1d2a935388d80fde05b65
jamieberrier <berrier.jamie@gmail.com>
2018-08-14 00:01:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -19,7 +19,6 @@ class Anagram
     anagram_array.each do |anagram|
       if anagram.split("").sort == @word.split("").sort
         matched_anagrams << anagram
-        #matched_anagrams
       else
         matched_anagrams
       end
