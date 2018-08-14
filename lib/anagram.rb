db243c7e97b48c10ccceab8c00a5afc8ede247f9
jamieberrier <berrier.jamie@gmail.com>
2018-08-14 00:06:39 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,8 +17,10 @@ class Anagram
     matched_anagrams = []
 
     anagram_array.each do |anagram|
-      anagram.split("").sort == @word.split("").sort ? matched_anagrams << anagram
+      anagram.split("").sort == @word.split("").sort ? matched_anagrams << anagram : matched_anagrams
     end
     matched_anagrams
   end
+
+  
 end
