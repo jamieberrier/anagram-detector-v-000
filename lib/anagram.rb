0a4322f931109c92581cf298020d390d32f4ba64
jamieberrier <berrier.jamie@gmail.com>
2018-08-14 00:04:50 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -19,5 +19,5 @@ class Anagram
     anagram_array.each do |anagram|
       anagram.split("").sort == @word.split("").sort ? matched_anagrams << anagram : matched_anagrams
     end
-
+    matched_anagrams
 end
